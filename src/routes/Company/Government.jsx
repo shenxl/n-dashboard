606cf76d230211a9e5f58de42ae81ee005ff81dc
lsy <lishuangyi431212@163.com>
2017-01-04 20:00:02 +0800

    修改页面样式

--- src/routes/Company/Government.jsx
+++ src/routes/Company/Government.jsx
@@ -83,7 +83,12 @@ const Government = ({ dispatch, companies, global, report, showdata, exportData 
       type: 'companies/setSubSelect',
       payload: { subSelect: 'subVersion' },
     });
-
+    // dispatch({
+    //   type: 'showdata/setModalState',
+    //   payload: {
+    //     showDataVisibel: true,
+    //   },
+    // });
     dispatch({
       type: 'report/setVersionTitle',
       payload: { versionTitle: title },
